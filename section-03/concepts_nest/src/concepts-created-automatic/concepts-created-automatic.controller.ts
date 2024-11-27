c94bf68b860249ee7d2b9bccb541982e3eafc7f9
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2024-11-27 16:28:38 -0300

    created provide automatic

--- section-03/concepts_nest/src/concepts-created-automatic/concepts-created-automatic.controller.ts
+++ section-03/concepts_nest/src/concepts-created-automatic/concepts-created-automatic.controller.ts
@@ -1,9 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ConceptsCreatedAutomaticService } from './concepts-created-automatic.service';
 
 @Controller('concepts-created-automatic')
 export class ConceptsCreatedAutomaticController {
+  constructor(
+    private readonly conceptsCreatedAutomaticService: ConceptsCreatedAutomaticService,
+  ) {}
+
   @Get()
   home() {
-    return 'Welcome to the concepts-created-automatic';
+    return this.conceptsCreatedAutomaticService.getHome();
   }
 }
