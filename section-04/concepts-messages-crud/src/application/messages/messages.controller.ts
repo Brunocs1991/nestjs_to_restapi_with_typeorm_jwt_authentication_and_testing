0d01b2c0e40d7861431ab600043374c1d44b3456
Bruno Costa e Silva <brunocosta.1991@hotmail.com>
2024-11-27 21:25:00 -0300

    query parameters

--- section-04/concepts-messages-crud/src/application/messages/messages.controller.ts
+++ section-04/concepts-messages-crud/src/application/messages/messages.controller.ts
@@ -8,13 +8,17 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
-  findAll() {
-    return 'This action returns all messages';
+  findAll(@Query() pagination: any) {
+    const { limit = 5, offset = 5 } = pagination;
+    return {
+      body: `return all messages. limit: ${limit}, offset: ${offset}`,
+    };
   }
 
   @Get(':id')
